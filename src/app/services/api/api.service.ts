53a9be61d53c8749d767f8c9b9d44316fcac068d
khaledCSE <khaledcse10@gmail.com>
2023-07-26 13:39:18 +0600

    ready for PROD

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { of, tap } from 'rxjs';
 import { ICrypto, IExchange } from '../../types';
+import { environment } from '../../../environments/environment'
 
 @Injectable({
   providedIn: 'root'
@@ -28,7 +29,9 @@ export class ApiService {
       params: {
         filter_asset_id: 'GBP,USD,EUR,CAD',
         invert: 'false',
-        apikey: 'BF22C406-7DC8-4E82-A7E3-ADA55BA263BA',
+      },
+      headers: {
+        "Authorization": environment.apiKey
       }
     })
     return res.pipe(tap((res) => this.exchangeCache.set(this.exchangeUrl, res)))
@@ -42,8 +45,8 @@ export class ApiService {
     }
 
     const res = this.http.get<ICrypto[]>(this.coinsUrl, {
-      params: {
-        apikey: 'BF22C406-7DC8-4E82-A7E3-ADA55BA263BA',
+      headers: {
+        "Authorization": environment.apiKey
       }
     })
     return res.pipe(tap((res) => this.coinsCache.set(this.coinsUrl, res)))
